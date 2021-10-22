3fc86f1ee5195521fa38d1c2772b51370478a04f
AA <86760749+aerocyber@users.noreply.github.com>
2021-10-22 14:42:17 +0530

    Create osmata.js
    Create outline of osmata.js

---
+++ osmata.js
@@ -0,0 +1,23 @@
+/**
+* Copyright ©️ 2021 aerocyber 
+* https://github.com/aerocyber/osmata.js
+* Licensed under the terms of MIT License. 
+*/ 
+
+// Feature Add Osmation
+function osmate(data, name, url, category) {}
+
+// Feature Remove Osmation 
+function deosmate(name, data){}
+
+// Feature Retrieve Osmata spec version followed 
+function getSpecVersion(){}
+
+// Feature Get Url by name
+function getUrl(name, data){}
+
+// Feature Edit name
+function editName(data, oldName, newName){}
+
+// Feature Edit Url
+function editUrl(data, name, newUrl){}
